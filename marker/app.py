0cf956b23f0d404287bf18f22e2078d3da6d691f
omarelsheekh <31706237+omarelsheekh@users.noreply.github.com>
2019-07-24 10:26:49 +0200

    fix requests

--- marker/app.py
+++ marker/app.py
@@ -1,36 +1,9 @@
 
 from flask import *
-import json,requests
+import requests
 from flask_oauth import OAuth
 from flask_github import GitHub
 from github import Github as git
-from peewee import *
-import pygments
-from pygments.lexers import PythonLexer
-from pygments.formatters import HtmlFormatter
-
-database = SqliteDatabase('app.db')
-
-class BaseMode(Model):
-    class Meta:
-        database = database
-
-class User(BaseMode):
-    username = CharField( unique = True)
-    password = CharField()
-    email = CharField()
-
-class Src(BaseMode):
-    text = TextField()
-    language  = CharField()
-    user = ForeignKeyField(User, backref='srcs')
-
-def create_tables():
-    database.create_tables([User, Src])
-
-def mark(text):
-    #styles >> HtmlFormatter().get_style_defs('.highlight')
-    return pygments.highlight(text, PythonLexer(), HtmlFormatter())
 
 # You must configure these 3 values from Google APIs console
 # https://code.google.com/apis/console
